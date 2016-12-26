6a3d2cfe57d61684da23207d9defc52242389e96
IvanShabanov <Ivan.shabanov@gmail.com>
2016-12-26 15:36:14 +0700

    Add files via upload

--- SimpleUpload.php
+++ SimpleUpload.php
@@ -1,14 +1,29 @@
 <?php
-    function SimpleUpload($fieldname, $path) {
+    /* Retunr html input field for files */
+    /* $fieldname - input name */
+    /* $multiple = false/true - if need multiple upload */
+    function FormField($fieldname, $multiple = false) {
+      if ($multiple) {
+         return '<input type="file" name="'.$fieldname.'[]" multiple />';
+      }
+      return '<input type="file" name="'.$fieldname.'" />';
+    }
+    
+    /* Return fieldnames array */
+    /* $fieldname - input name */
+    /* $path - upload directory */
+    /* $savenames - false/true save origin file names */
+    function SimpleUpload($fieldname, $path, $savenames = false) {
        $Result = array();
        if (is_array($_FILES[$fieldname]["tmp_name"])) {
          foreach ($_FILES[$fieldname]["tmp_name"] as $key => $value) {
            if (true) {
                $tmp_name = $_FILES[$fieldname]["tmp_name"][$key];
                $name = basename($_FILES[$fieldname]["name"][$key]);
-               $hash = substr(md5(uniqid(microtime())), 1, 16);
-               $name = $hash . strrchr(strtolower($name), '.');
-
+               if (!$savenames) { 
+                 $hash = substr(md5(uniqid(microtime())), 1, 16);
+                 $name = $hash . strrchr(strtolower($name), '.');
+               } 
                $isloaded = true;
                If (!move_uploaded_file($tmp_name, $path.$name) ) {
                    If (!copy($tmp_name, $path.$name) ) {
@@ -28,8 +43,10 @@
            if (!empty($_FILES[$fieldname]["tmp_name"])) {
                $tmp_name = $_FILES[$fieldname]["tmp_name"];
                $name = basename($_FILES[$fieldname]["name"]);
-               $hash = substr(md5(uniqid(microtime())), 1, 16);
-               $name = $hash . strrchr(strtolower($name), '.');
+               if (!$savenames) { 
+                 $hash = substr(md5(uniqid(microtime())), 1, 16);
+                 $name = $hash . strrchr(strtolower($name), '.');
+               };
                $isloaded = true;
                If (!move_uploaded_file($tmp_name, $path.$name) ) {
                    If (!copy($tmp_name, $path.$name) ) {
