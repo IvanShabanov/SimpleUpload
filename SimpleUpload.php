df24eefb73577a00793384356e34f508a8ca2b64
Ivan Shabanov <Ivan.shabanov@gmail.com>
2021-02-26 12:50:16 +0700

    Update SimpleUpload.php

--- SimpleUpload.php
+++ SimpleUpload.php
@@ -11,7 +11,7 @@
       return '<input type="file" name="'.$fieldname.'" '.$fieldoptions.' />';
     }
     
-    /* Return   
+    /* Return
         array(
             [] => array(
                 'original' - original filename
@@ -23,73 +23,32 @@
             )
         )
     */
-    /* $fieldname - input name */
-    /* $path - upload directory */
-    /* $savenames - false/true save origin file names */
-    /* $avalable_extensions = array () */
-    function SimpleUpload($fieldname, $path, $savenames = false, $avalable_extensions = null) {
-        $Result = array();
-        if (substr($path, 0, -1) != '/') {
-            $path .= '/';
-        };
-        if (is_array($_FILES[$fieldname]["tmp_name"])) {
-            foreach ($_FILES[$fieldname]["tmp_name"] as $key => $value) {
-                if (!empty($_FILES[$fieldname]["tmp_name"][$key])) {
-                    $upload_this = true;
-                    $file = array();
-                    $file['original'] = basename($_FILES[$fieldname]["name"][$key]);
-
-                    $file['uploaded'] = $file['original'];
-                    $extension = explode(".", $file['original']);
-                    $extension = end($extension);
-                    $extension = mb_strtolower($extension);
-                    if (!$savenames) {
-                        $hash = substr(md5(uniqid(microtime())), 1, 16);
-                        $file['uploaded'] = $hash.'.'.$extension;
-                    }
-                    $file['full_path'] = $path.$file['uploaded'];
-
-                    if (!is_null($avalable_extensions)) {
-                        if (!is_array($avalable_extensions)) {
-                            $avalable_extensions = explode(',', $avalable_extensions);
-                        };
-                        if (is_array($avalable_extensions)) {
-                            if (!in_array($extension, $avalable_extensions)) {
-                                $upload_this = false;
-                                $Result['errors'][] = 'Not avalable extension file: '.$file['original'];
-                            }
-                        }
-                    };
-                    if ($upload_this) {
-                        $tmp_name = $_FILES[$fieldname]["tmp_name"][$key];
-                        $isloaded = true;
-                        if (!move_uploaded_file($tmp_name, $file['full_path'])) {
-                            if (!copy($tmp_name, $file['full_path'])) {
-                                $isloaded = false;
-                            }
-                        }
-                        if ($isloaded) {
-                            $Result[] = $file;
-                        } else {
-                            $Result['errors'][] = 'Error to load: '.$file['original'];
-                        };
-                    };
-                };
-            };
-        } else {
-            if (!empty($_FILES[$fieldname]["tmp_name"])) {
+/* $fieldname - input name */
+/* $dir - upload directory */
+/* $savenames - false/true save origin file names */
+/* $avalable_extensions = array () */
+/* $pattern - preg выражение по которому проверяется допустимость файла */
+function SimpleUpload($fieldname, $dir, $savenames = false, $avalable_extensions = null, $pattern = null) {
+    $Result = array();
+    if (substr($dir, 0, -1) != '/') {
+        $dir .= '/';
+    };
+    if (is_array($_FILES[$fieldname]["tmp_name"])) {
+        foreach ($_FILES[$fieldname]["tmp_name"] as $key => $value) {
+            if (!empty($_FILES[$fieldname]["tmp_name"][$key])) {
                 $upload_this = true;
                 $file = array();
-                $file['original'] = basename($_FILES[$fieldname]["name"]);
+                $file['original'] = basename($_FILES[$fieldname]["name"][$key]);
+
                 $file['uploaded'] = $file['original'];
                 $extension = explode(".", $file['original']);
                 $extension = end($extension);
                 $extension = mb_strtolower($extension);
                 if (!$savenames) {
                     $hash = substr(md5(uniqid(microtime())), 1, 16);
-                    $file['uploaded'] = $hash.'.'.$extension;
+                    $file['uploaded'] = $hash . '.' . $extension;
                 }
-                $file['full_path'] = $path.$file['uploaded'];
+                $file['full_path'] = $dir . $file['uploaded'];
 
                 if (!is_null($avalable_extensions)) {
                     if (!is_array($avalable_extensions)) {
@@ -98,12 +57,19 @@
                     if (is_array($avalable_extensions)) {
                         if (!in_array($extension, $avalable_extensions)) {
                             $upload_this = false;
-                            $Result['errors'][] = 'Not avalable extension file: '.$file['original'];
+                            $Result['errors'][] = 'Не допустимый тип файла: ' . $file['original'];
                         }
+                    }
+
+                };
+                if (!is_null($pattern)) {
+                    if (!preg_match($pattern, $file['uploaded'])) {
+                        $upload_this = false;
+                        $Result['errors'][] = 'Не допустимые символы в имени файла: ' . $file['original'];
                     }
                 };
                 if ($upload_this) {
-                    $tmp_name = $_FILES[$fieldname]["tmp_name"];
+                    $tmp_name = $_FILES[$fieldname]["tmp_name"][$key];
                     $isloaded = true;
                     if (!move_uploaded_file($tmp_name, $file['full_path'])) {
                         if (!copy($tmp_name, $file['full_path'])) {
@@ -113,10 +79,58 @@
                     if ($isloaded) {
                         $Result[] = $file;
                     } else {
-                        $Result['errors'][] = 'Error to load: '.$file['original'];
+                        $Result['errors'][] = 'Ошибка загрузки: ' . $file['original'];
                     };
                 };
-            };            
-        };            
-        return $Result;
-     }   
+            };
+        };
+    } else {
+        if (!empty($_FILES[$fieldname]["tmp_name"])) {
+            $upload_this = true;
+            $file = array();
+            $file['original'] = basename($_FILES[$fieldname]["name"]);
+            $file['uploaded'] = $file['original'];
+            $extension = explode(".", $file['original']);
+            $extension = end($extension);
+            $extension = mb_strtolower($extension);
+            if (!$savenames) {
+                $hash = substr(md5(uniqid(microtime())), 1, 16);
+                $file['uploaded'] = $hash . '.' . $extension;
+            }
+            $file['full_path'] = $dir . $file['uploaded'];
+
+            if (!is_null($avalable_extensions)) {
+                if (!is_array($avalable_extensions)) {
+                    $avalable_extensions = explode(',', $avalable_extensions);
+                };
+                if (is_array($avalable_extensions)) {
+                    if (!in_array($extension, $avalable_extensions)) {
+                        $upload_this = false;
+                        $Result['errors'][] = 'Не допустимый тип файла: ' . $file['original'];
+                    }
+                }
+            };
+            if (!is_null($pattern)) {
+                if (!preg_match($pattern, $file['uploaded'])) {
+                    $upload_this = false;
+                    $Result['errors'][] = 'Не допустимые символы в имени файла: ' . $file['original'];
+                }
+            };
+            if ($upload_this) {
+                $tmp_name = $_FILES[$fieldname]["tmp_name"];
+                $isloaded = true;
+                if (!move_uploaded_file($tmp_name, $file['full_path'])) {
+                    if (!copy($tmp_name, $file['full_path'])) {
+                        $isloaded = false;
+                    }
+                }
+                if ($isloaded) {
+                    $Result[] = $file;
+                } else {
+                    $Result['errors'][] = 'Ошибка загрузки: ' . $file['original'];
+                };
+            };
+        };
+    };
+    return $Result;
+}
